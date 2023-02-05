40c1846113013703a4d2764032c8e13fe98b8c1d
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-02-05 15:32:47 -0500

    feat: husky and eslint

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,3 @@
-import mongoose from 'mongoose'
+const connectDB = (): void => {};
 
-const connectDB = (): void => {
-  const DB_URL = process.env.DB_URL as string
-
-  mongoose.set('strictQuery', true)
-  mongoose.connect(DB_URL)
-  .then(() => {
-    console.log('db connected...')
-  })
-  .catch(e => {
-    console.log(e)
-  })
-}
-
-export default connectDB
+export default connectDB;
